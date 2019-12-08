8df7caf3ceecedc7cf8f70177f0d36d3138b505b
Alex Wang <alexander.gj.wang@gmail.com>
2019-12-07 22:35:49 -0500

    fix cml args

--- generate_fake_images.py
+++ generate_fake_images.py
@@ -29,10 +29,12 @@ def plot_images(args, x_sample, dir, file_name, size_x=3, size_y=3, input_size=(
         else:
             plt.imshow(sample)
 
-    plt.savefig(dir + file_name + '.png', bbox_inches='tight')
+    plt.savefig(path + file_name + '.png', bbox_inches='tight')
     plt.close(fig)
 
 path = sys.argv[0]
+folder1 = sys.argv[1]
+folder2 = sys.argv[2]
 
 BATCH_SIZE = 100
 
@@ -43,6 +45,7 @@ path = "/content/vae_vampprior/"
 input_size = [1, 28, 20]
 
 # start processing
+# TODO: make this modular
 with open('datasets/Freyfaces/freyfaces.pkl', 'rb') as f:
     data = pickle.load(f, encoding='latin1')
 
@@ -64,13 +67,13 @@ for batch_idx, (data, target) in enumerate(test_loader):
 
     x = data
 
-    if not os.path.exists(path + '/test1/'):
-        os.makedirs(path + '/test1/')
+    if not os.path.exists(path + "/{}/".format(folder1)):
+        os.makedirs(path + "/{}/".format(folder1))
 
-    if not os.path.exists(dir + '/test2/'):
-        os.makedirs(path + '/test2/')
+    if not os.path.exists(path + "/{}/".format(folder2)):
+        os.makedirs(path + "/{}/".format(folder2))
 
-    plot_images(args, data.data.cpu().numpy()[0:9], dir + 'test1/', 'real{}'.format(batch_idx), 3, 3, input_size)
+    plot_images(args, data.data.cpu().numpy()[0:9], path + "/{}/".format(folder1), 'real{}'.format(batch_idx), 3, 3, input_size)
     x_mean = model.reconstruct_x(x)
-    plot_images(args, x_mean.data.cpu().numpy()[0:9], dir + 'test2/', 'fake{}'.format(batch_idx), 3, 3, input_size)
+    plot_images(args, x_mean.data.cpu().numpy()[0:9], path + "/{}/".format(folder2), 'fake{}'.format(batch_idx), 3, 3, input_size)
 
